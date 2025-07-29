613ec31933ccb9b2b8e50ff732d0cb0b3a2f1f11
chakulama <chaku.lama@gmail.com>
2025-07-29 16:00:03 +0200

    update manaully

---
+++ _nuxt/4iLyWKRu.js
@@ -0,0 +1,1 @@
+import{g as e}from"./BJQMldk8.js";function s(){const{t:o,locale:t,tm:n}=e();return{t:o,locale:t,tm:n}}export{s as u};
--- _nuxt/BRRlNnNH.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./B8W25jdo.js";function s(){const{t:o,locale:t,tm:n}=e();return{t:o,locale:t,tm:n}}export{s as u};
